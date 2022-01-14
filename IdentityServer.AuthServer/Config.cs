b1a7cdcf546c32f8574e81098e4e62b46495fd47
Ömer Faruk Şahin <ofaruksahin@outlook.com.tr>
2022-01-14 22:49:52 +0300

    Genel proje mimarisi kuruldu. IdentityServer4 paketi kuruldu, client configler tanımlandı

---
+++ IdentityServer.AuthServer/Config.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace IdentityServer.AuthServer
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("resource_api1")
+                {
+                    Scopes = new List<string>{"api1.read","api1.write","api1.update"}
+                },
+                new ApiResource("resource_api2")
+                {
+                    Scopes = new List<string>{"api2.read","api2.write","api2.update"}
+                }
+            };
+        }
+
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new List<ApiScope>
+            {
+                new ApiScope("api1.read","API 1 için okuma izni"),
+                new ApiScope("api1.write","API 1 için yazma izni"),
+                new ApiScope("api1.update","API 1 için güncelleme izni"),
+                new ApiScope("api2.read","API 2 için okuma izni"),
+                new ApiScope("api2.write","API 2 için yazma izni"),
+                new ApiScope("api2.update","API 2 için güncelleme izni")
+            };
+        }
+    }
+}
---
+++ IdentityServer.AuthServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace IdentityServer.AuthServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
